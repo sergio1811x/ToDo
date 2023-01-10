e1686ff1f0c3ed549db29df028cb636e332f6559
Sergio Lazarkov <97618337+sergio1811x@users.noreply.github.com>
2023-01-10 20:15:10 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,60 @@
-import s from './App.module.css';
-import React, {useState} from "react";
+import s from "./App.module.css";
+import React, { useRef, useState } from "react";
 import FieldMain from "./Components/field";
 import List from "./Components/list";
 
+function App() {
+  const [value, setValue] = useState([]);
+  const [obj, setObj] = useState("");
+  const [todo, setTodo] = useState(value);
 
-function App(props) {
-    const [value, setValue] = useState([])
-    const [obj, setObj] = useState([]);
-    const [todo, setTodo] = useState(value)
+  const change = (message, e) => {
+    setObj({ [message]: e.target.value });
+  };
 
-    const change = (message, e) => {
-        setObj({[message]: e.target.value});
+  const add = () => {
+    let num = Math.floor(Math.random() * 10000) + 1;
+    if (obj.message.length > 0) {
+      setValue([...value, { message: obj.message, id: num }]);
     }
+  };
 
-    const add = () => {
-        let num = (Math.floor(Math.random() * 10000) + 1)
-        if (obj.message.length > 0) {
-        setValue([...value, {message: obj.message, id: num}])
-       }
+  const del = (id) => {
+    setValue(value.filter((obj) => obj.id != id));
+  };
+
+  const delAll = () => {
+    setValue([]);
+  };
+
+  const complete = (index) => {
+    const newTodos = [...value];
+    if (!newTodos[index].isCompleted) {
+      newTodos[index].isCompleted = true;
+    } else {
+      newTodos[index].isCompleted = false;
     }
+    setValue(newTodos);
+  };
 
-    const del = (id) => {
-        setValue(value.filter(obj => obj.id != id));
-    }
-
-    const delAll = () => {
-        setValue([])
-    }
-
-    const complete = (index) => {
-        const newTodos = [...value];
-        if (!newTodos[index].isCompleted) {
-            newTodos[index].isCompleted = true;
-        } else {
-            newTodos[index].isCompleted = false;
-        }
-        setValue(newTodos);
-    };
-
-
-    return (
-        <div className={s.main}>
-            <div className={s.app}>
-                <div className={s.h1}>
-                    <h1>Список дел</h1>
-                </div>
-                <FieldMain sendMessage={props.sendMessage}
-                           add={add}
-                           change={change}/>
-                <List messagesPage={props.messagesPage}
-                      delete={props.delete}
-                      value={value}
-                      setValue={setValue}
-                      del={del}
-                      complete={complete}
-                      todo={todo}
-                      delAll={delAll}/>
-            </div>
+  return (
+    <div className={s.main}>
+      <div className={s.app}>
+        <div className={s.h1}>
+          <h1>Список дел</h1>
         </div>
-    );
+        <FieldMain add={add} obj={obj} change={change} setObj={setObj} />
+        <List
+          value={value}
+          setValue={setValue}
+          del={del}
+          complete={complete}
+          todo={todo}
+          delAll={delAll}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
-
-
+export default App;
